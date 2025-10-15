70729a09679fb2efa4d4ba830e00cd864542bbe3
Grobzi <c.schnurr@gmx.net>
2025-10-15 23:49:56 +0200

    Added formatting. Added enable pin logic.

--- src/PsuPowerMonitor/PsuPowerMonitor.ino
+++ src/PsuPowerMonitor/PsuPowerMonitor.ino
@@ -6,14 +6,15 @@
 #include <Wire.h>
 
 #define TouchScreen TouchScreen_kbv
-#define TSPoint     TSPoint_kbv
+#define TSPoint TSPoint_kbv
 #define XP 8
 #define YP A3
 #define XM A2
 #define YM 9
+#define ENABLE_PIN D1
 
 Psu psu_0 = Psu(0x5F);
-Psu psu_1 = Psu(0x5F);
+Psu psu_1 = Psu(0x5C);
 MCUFRIEND_kbv tft;
 TouchScreen ts(8, YP, XM, YM, 300);
 TSPoint tp;
@@ -27,22 +28,23 @@ ChangeableText power_combined_text = ChangeableText(tft, 180, 175, 1, WHITE, GRA
 
 void setup()
 {
-  Wire.setTimeout(10);
-  Wire.begin();
-  Serial.begin(9600);
-  //while (!Serial);
-  tft.begin();
-  tft.setRotation(1);
-  tft.cp437(true);
+    Wire.setTimeout(10);
+    Wire.begin();
+    Serial.begin(9600);
+    pinMode(ENABLE_PIN, PinMode::OUTPUT);
+    // while (!Serial);
+    tft.begin();
+    tft.setRotation(1);
+    tft.cp437(true);
 }
 
 // State machine variables
 enum State
 {
-  Init,
-  Overview,
-  Detail,
-  Settings
+    Init,
+    Overview,
+    Detail,
+    Settings
 };
 State state = State::Init;
 State last_state = State::Init;
@@ -50,202 +52,228 @@ int8_t selected_psu;
 
 void display_loop()
 {
-  static uint32_t last_draw_time = 0;
-  uint32_t time = millis();
-  if(last_state == state)
-  {
-    if(time < last_draw_time)
-      last_draw_time = 0;
-    if(time - last_draw_time < 500)
-      return;
-  }
-  last_draw_time = time;
-
-  if(state == State::Init)
-  {
-    last_state = state;
-    tft.fillScreen(RED);
-    //tft.setCursor(10, 10);
-    //tft.setTextColor(WHITE);  
-    //tft.setTextSize(1);
-    //tft.print("Read PSU 1 information... ");
-    //psu_0.update_device_information();
-    //tft.print(psu_0.connected() ? psu_0.spn.c_str() : "FAILED");
-    //tft.setCursor(10, 20);
-    //tft.print("Read PSU 2 information.");
-    //psu_1.update_device_information();
-    //tft.print(psu_1.connected() ? psu_1.spn.c_str() : "FAILED");
-    tft.fillScreen(WHITE);
-    state = State::Overview;
-  }
-
-  if(state == State::Overview)
-  {
-    if(last_state != state)
+    static uint32_t last_draw_time = 0;
+    uint32_t time = millis();
+    if (last_state == state)
     {
-      last_state = state;
-      draw_banner(tft, "UBERSICHT");
-      tft.fillRect(0, 25, tft.width(), tft.height(), WHITE);
-      tft.fillRect(tft.width() / 2 - 1, 25, 3, 70, GRAY);
-      tft.fillRect(0, 95, tft.width(), 3, GRAY);
-      tft.fillRect(0, 155, tft.width(), 100, GRAY);
-      Serial.println("Main layout drawn");
-      view_0.set_state(PsuState::error);
-      view_1.set_state(PsuState::error);
+        if (time < last_draw_time)
+            last_draw_time = 0;
+        if (time - last_draw_time < 500)
+            return;
+    }
+    last_draw_time = time;
+
+    if (state == State::Init)
+    {
+        last_state = state;
+        tft.fillScreen(RED);
+        tft.setCursor(10, 10);
+        tft.setTextColor(WHITE);
+        tft.setTextSize(1);
+        tft.print("Connect to PSU 1... ");
+        delay(100);
+        psu_0.connect();
+        tft.print(psu_0.connected() ? psu_0.spn.c_str() : "FAILED");
+        tft.setCursor(10, 20);
+        tft.print("Connect to PSU 2... ");
+        delay(100);
+        psu_1.connect();
+        tft.print(psu_1.connected() ? psu_1.spn.c_str() : "FAILED");
+        delay(100);
+        if (!psu_0.connected() || !psu_1.connected())
+        {
+            delay(500);
+        }
+
+        state = State::Overview;
     }
 
-    float combined_voltage = 0;
-    float combined_current = 0;
-    float combined_power = 0;
-    float combined_power_used = 0;
-    for(int i = 0; i <= 1; i++)
+    if (state == State::Overview)
     {
-      PsuView * view = &view_0;
-      Psu * psu = &psu_0;
-      if(i == 1)
-      {
-        view = &view_1;
-        psu = &psu_1;
-      }
-      if(psu->connected())
-      {
-        combined_voltage += psu->voltage_out();
-        combined_current += psu->current_out();
-        combined_power += psu->power_in();
-        combined_power_used += psu->power_consumed();
-        view->set_state(PsuState::normal);
-        view->initial_draw();
-        view->update_draw(format_precision(psu->voltage_out(), 2), 
-                          format_precision(psu->current_out(), 1),
-                          format_precision(psu->temperature_internal(), 1),
-                          format_precision(psu->power_in(), 0));
-      }
-      else
-      {
-        view->set_state(PsuState::error);
-        view->initial_draw();
-      }
+        if (last_state != state)
+        {
+            last_state = state;
+            draw_banner(tft, "UBERSICHT");
+            tft.fillRect(0, 25, tft.width(), tft.height(), WHITE);
+            tft.fillRect(tft.width() / 2 - 1, 25, 3, 70, GRAY);
+            tft.fillRect(0, 95, tft.width(), 3, GRAY);
+            tft.fillRect(0, 155, tft.width(), 100, GRAY);
+            Serial.println("Main layout drawn");
+            view_0.set_state(PsuState::error);
+            view_1.set_state(PsuState::error);
+        }
+
+        float combined_voltage = 0;
+        float combined_current = 0;
+        float combined_power = 0;
+        float combined_power_used = 0;
+        for (int i = 0; i <= 1; i++)
+        {
+            PsuView *view = &view_0;
+            Psu *psu = &psu_0;
+            if (i == 1)
+            {
+                view = &view_1;
+                psu = &psu_1;
+            }
+            if (psu->connected())
+            {
+                combined_voltage += psu->voltage_out();
+                combined_current += psu->current_out();
+                combined_power += psu->power_in();
+                combined_power_used += psu->power_consumed();
+                view->set_state(PsuState::normal);
+                view->initial_draw();
+                view->update_draw(format_precision(psu->voltage_out(), 2), format_precision(psu->current_out(), 1),
+                                  format_precision(psu->temperature_internal(), 1),
+                                  format_precision(psu->power_in(), 0));
+            }
+            else
+            {
+                view->set_state(PsuState::error);
+                view->initial_draw();
+            }
+        }
+
+        voltage_text.set_text(format_precision(combined_voltage, 2) + "V");
+        current_text.set_text(format_precision(combined_current / 2, 1) + "A");
+        int16_t x1, y1;
+        uint16_t w, h;
+        std::string text = format_precision(combined_power, 0) + "W";
+        tft.setTextSize(7);
+        tft.getTextBounds(text.c_str(), 0, 0, &x1, &y1, &w, &h);
+        power_text.set_text(text, (tft.width() - w) / 2, 175);
+
+        text = format_precision(combined_power_used / 1000, 2) + "kWh";
+        tft.setTextSize(1);
+        tft.getTextBounds(text.c_str(), 0, 0, &x1, &y1, &w, &h);
+        power_combined_text.set_text(text, (tft.width() - w - 5), 230);
     }
 
-    voltage_text.set_text(format_precision(combined_voltage, 2) + "V");
-    current_text.set_text(format_precision(combined_current / 2, 1) + "A");
-    int16_t x1, y1;
-    uint16_t w, h;
-    std::string text = format_precision(combined_power, 0) + "W";
-    tft.setTextSize(7);
-    tft.getTextBounds(text.c_str(), 0, 0, &x1, &y1, &w, &h);
-    power_text.set_text(text, (tft.width() - w) / 2, 175);
-
-    text = format_precision(combined_power_used / 1000, 2) + "kWh";
-    tft.setTextSize(1);
-    tft.getTextBounds(text.c_str(), 0, 0, &x1, &y1, &w, &h);
-    power_combined_text.set_text(text, (tft.width() - w - 5), 230);
-  }
-
-  if(state == State::Detail)
-  {
-    if(last_state != state)
+    if (state == State::Detail)
     {
-      last_state = state;
-      if(selected_psu == 0)
-        draw_banner(tft, "DETAILS PSU 1");
-      if(selected_psu == 1)
-        draw_banner(tft, "DETAILS PSU 2");
+        if (last_state != state)
+        {
+            last_state = state;
+            detail_view.invalidate();
+            if (selected_psu == 0)
+                draw_banner(tft, "DETAILS PSU 1");
+            if (selected_psu == 1)
+                draw_banner(tft, "DETAILS PSU 2");
+            detail_view.initial_draw();
+        }
+        Psu *psu = &psu_0;
+        if (selected_psu == 1)
+            psu = &psu_1;
+        if (psu->connected())
+            detail_view.update_draw(
+                format_precision(psu->voltage_in(), 2), format_precision(psu->voltage_out(), 2),
+                format_precision(psu->current_in(), 1), format_precision(psu->current_out(), 1),
+                format_precision(psu->power_in(), 0), format_precision(psu->power_out(), 0),
+                format_precision(psu->temperature_intake(), 1), format_precision(psu->temperature_internal(), 1),
+                format_precision(psu->fan_speed(), 0), format_precision(psu->seconds_since_startup(), 0),
+                format_precision(psu->power_consumed(), 0));
     }
-    detail_view.initial_draw();
-    Psu * psu = &psu_0;
-    if(selected_psu == 1)
-      psu = &psu_1;
-    if(psu->voltage_in() > 0)
-      detail_view.update_draw(format_precision(psu->voltage_in(), 2), 
-                              format_precision(psu->voltage_out(), 2),
-                              format_precision(psu->current_in(), 1),
-                              format_precision(psu->current_out(), 1),
-                              format_precision(psu->power_in(), 0),
-                              format_precision(psu->power_out(), 0),
-                              format_precision(psu->temperature_intake(), 1),
-                              format_precision(psu->temperature_internal(), 1),
-                              format_precision(psu->fan_speed(), 0),
-                              format_precision(psu->seconds_since_startup(), 0),
-                              format_precision(psu->power_consumed(), 0));
-  }
 }
 
 void touch_loop()
 {
-  auto p = ts.getPoint();
-  //restore shared pins
-  pinMode(YP, OUTPUT);      
-  pinMode(XM, OUTPUT);
-  digitalWrite(YP, HIGH);
-  digitalWrite(XM, HIGH);
-  if(p.z < 10) return;
-  auto x = map(p.y, 913, 165, 0, tft.width());
-  auto y = map(p.x, 172, 846, 0, tft.height());
-  Serial.print("Touch on: ");
-  Serial.print(x);
-  Serial.print("x");
-  Serial.println(y);
-  if(state == State::Init)
-  {
-    //No touch operation
-  }
+    auto p = ts.getPoint();
+    // restore shared pins
+    pinMode(YP, OUTPUT);
+    pinMode(XM, OUTPUT);
+    digitalWrite(YP, HIGH);
+    digitalWrite(XM, HIGH);
+    if (p.z < 10)
+        return;
+    auto x = map(p.y, 913, 165, 0, tft.width());
+    auto y = map(p.x, 172, 846, 0, tft.height());
+    Serial.print("Touch on: ");
+    Serial.print(x);
+    Serial.print("x");
+    Serial.println(y);
+    if (state == State::Init)
+    {
+        // No touch operation
+    }
 
-  if(state == State::Overview)
-  {
-    if(x > 0 && x < 150 &&
-       y > 30 && y < 80)
-       {
-          selected_psu = 0;
-          state = State::Detail;
-          detail_view.invalidate();
-          display_loop();
-       }
-    if(x > 210 && x < 300 &&
-       y > 30 && y < 80)
-       {
-          selected_psu = 1;
-          state = State::Detail;
-          detail_view.invalidate();
-          display_loop();
-       }
-  }
+    if (state == State::Overview)
+    {
+        if (x > 0 && x < 150 && y > 30 && y < 80)
+        {
+            selected_psu = 0;
+            state = State::Detail;
+            display_loop();
+        }
+        if (x > 210 && x < 300 && y > 30 && y < 80)
+        {
+            selected_psu = 1;
+            state = State::Detail;
+            display_loop();
+        }
+    }
 
-  if(state == State::Detail)
-  {
-    if(y > 0 && y < 40)
-       {
-          selected_psu = -1;
-          state = State::Overview;
-          current_text.set_text("");
-          voltage_text.set_text("");
-          power_text.set_text("");
-          power_combined_text.set_text("");
-          view_0.invalidate();
-          view_1.invalidate(); 
-          display_loop();
-       }
-  }
+    if (state == State::Detail)
+    {
+        if (y > 0 && y < 40)
+        {
+            selected_psu = -1;
+            state = State::Overview;
+            current_text.set_text("");
+            voltage_text.set_text("");
+            power_text.set_text("");
+            power_combined_text.set_text("");
+            view_0.invalidate();
+            view_1.invalidate();
+            display_loop();
+        }
+    }
+}
+
+uint32_t output_error_time = 0;
+bool output_error = false;
+
+void enable_signal_loop()
+{
+    if (psu_0.connected() && psu_0.voltage_in() > 0 && psu_1.connected() && psu_1.voltage_in() > 0)
+    {
+        if (!output_error)
+            digitalWrite(ENABLE_PIN, PinStatus::HIGH);
+
+        // Make sure that  300ms after the outputs are enabled the PSUs outputs are active
+        // If not switch the outputs off again.
+        if (psu_0.voltage_out() <= 0 || psu_1.voltage_out() <= 0)
+        {
+            if (output_error_time == 0)
+            {
+                output_error_time = millis();
+            }
+            if (millis() - output_error_time > 300)
+            {
+                digitalWrite(ENABLE_PIN, PinStatus::LOW);
+                output_error = true;
+            }
+        }
+        else
+        {
+            output_error_time = 0;
+        }
+    }
+    else
+    {
+        digitalWrite(ENABLE_PIN, PinStatus::LOW);
+    }
 }
 
 void loop()
 {
-  uint32_t start = millis();
+    uint32_t start = millis();
+    enable_signal_loop();
+    display_loop();
+    touch_loop();
 
-  if(psu_0.connected() && psu_1.connected())
-  {
-    // Enable signal
-  }
-  else
-  {
-    // Disable signal
-  }
-
-  display_loop();
-  touch_loop();
-
-  uint32_t end = millis();
-  //Serial.print("Main loop ms:");
-  //Serial.println(std::to_string(end - start).c_str());
-  delay(5);
+    uint32_t end = millis();
+    // Serial.print("Main loop ms:");
+    // Serial.println(std::to_string(end - start).c_str());
+    delay(5);
 }
--- src/PsuPowerMonitor/TouchScreen_kbv.cpp
+++ src/PsuPowerMonitor/TouchScreen_kbv.cpp
@@ -6,36 +6,42 @@
 
 #include "TouchScreen_kbv.h"
 
-#define NUMSAMPLES 3  //.kbv
-#if defined(__STM32F1__) || defined(ESP32)   //Maple core
-#define ADC_ADJUST >>2
+#define NUMSAMPLES 3                       //.kbv
+#if defined(__STM32F1__) || defined(ESP32) // Maple core
+#define ADC_ADJUST >> 2
 #else
 #define ADC_ADJUST
 #endif
 
-TSPoint_kbv::TSPoint_kbv(void) {
+TSPoint_kbv::TSPoint_kbv(void)
+{
     x = y = 0;
 }
 
-TSPoint_kbv::TSPoint_kbv(int16_t x0, int16_t y0, int16_t z0) {
+TSPoint_kbv::TSPoint_kbv(int16_t x0, int16_t y0, int16_t z0)
+{
     x = x0;
     y = y0;
     z = z0;
 }
 
-bool TSPoint_kbv::operator==(TSPoint_kbv p1) {
-    return  ((p1.x == x) && (p1.y == y) && (p1.z == z));
+bool TSPoint_kbv::operator==(TSPoint_kbv p1)
+{
+    return ((p1.x == x) && (p1.y == y) && (p1.z == z));
 }
 
-bool TSPoint_kbv::operator!=(TSPoint_kbv p1) {
-    return  ((p1.x != x) || (p1.y != y) || (p1.z != z));
+bool TSPoint_kbv::operator!=(TSPoint_kbv p1)
+{
+    return ((p1.x != x) || (p1.y != y) || (p1.z != z));
 }
 
-static void insert_sort(int array[], uint8_t size) {
+static void insert_sort(int array[], uint8_t size)
+{
     uint8_t j;
     int save;
 
-    for (int i = 1; i < size; i++) {
+    for (int i = 1; i < size; i++)
+    {
         save = array[i];
         for (j = i; j >= 1 && save < array[j - 1]; j--)
             array[j] = array[j - 1];
@@ -43,7 +49,8 @@ static void insert_sort(int array[], uint8_t size) {
     }
 }
 
-TSPoint_kbv TouchScreen_kbv::getPoint(void) {
+TSPoint_kbv TouchScreen_kbv::getPoint(void)
+{
     int x, y, z;
     int samples[NUMSAMPLES];
     uint8_t i, valid;
@@ -61,23 +68,25 @@ TSPoint_kbv TouchScreen_kbv::getPoint(void) {
     digitalWrite(_xp, HIGH);
     digitalWrite(_xm, LOW);
 
-    for (i = 0; i < NUMSAMPLES; i++) {
+    for (i = 0; i < NUMSAMPLES; i++)
+    {
         samples[i] = analogRead(_yp) ADC_ADJUST;
     }
     insert_sort(samples, NUMSAMPLES);
-    x = (1023 - samples[NUMSAMPLES / 2]); //choose median
+    x = (1023 - samples[NUMSAMPLES / 2]); // choose median
 
     pinMode(_xp, INPUT);
     pinMode(_xm, INPUT);
     digitalWrite(_xp, LOW);
-    digitalWrite(_xm, LOW);     //.kbv for Due
+    digitalWrite(_xm, LOW); //.kbv for Due
 
     pinMode(_yp, OUTPUT);
     digitalWrite(_yp, HIGH);
     pinMode(_ym, OUTPUT);
-    digitalWrite(_ym, LOW);     //.kbv for Due
+    digitalWrite(_ym, LOW); //.kbv for Due
 
-    for (i = 0; i < NUMSAMPLES; i++) {
+    for (i = 0; i < NUMSAMPLES; i++)
+    {
         samples[i] = analogRead(_xm) ADC_ADJUST;
     }
 
@@ -90,12 +99,12 @@ TSPoint_kbv TouchScreen_kbv::getPoint(void) {
     digitalWrite(_xp, LOW);
 
     // Set Y- to VCC
-    pinMode(_ym, OUTPUT);     //.kbv
+    pinMode(_ym, OUTPUT); //.kbv
     digitalWrite(_ym, HIGH);
 
     // Hi-Z X- and Y+
-    digitalWrite(_xm, LOW);   //.kbv
-    pinMode(_xm, INPUT);      //.kbv
+    digitalWrite(_xm, LOW); //.kbv
+    pinMode(_xm, INPUT);    //.kbv
     digitalWrite(_yp, LOW);
     pinMode(_yp, INPUT);
 
@@ -104,10 +113,11 @@ TSPoint_kbv TouchScreen_kbv::getPoint(void) {
 
     z = (1023 - (z2 - z1));
 
-    return TSPoint_kbv(x, y, z);  //XM, YP still in ANALOG mode
+    return TSPoint_kbv(x, y, z); // XM, YP still in ANALOG mode
 }
 
-TouchScreen_kbv::TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym) {
+TouchScreen_kbv::TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym)
+{
     _yp = yp;
     _xm = xm;
     _ym = ym;
@@ -116,9 +126,8 @@ TouchScreen_kbv::TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym)
     pressureThreshhold = 10;
 }
 
-
-TouchScreen_kbv::TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym,
-                         uint16_t rxplate) {
+TouchScreen_kbv::TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym, uint16_t rxplate)
+{
     _yp = yp;
     _xm = xm;
     _ym = ym;
@@ -128,7 +137,8 @@ TouchScreen_kbv::TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym,
     pressureThreshhold = 10;
 }
 
-int TouchScreen_kbv::readTouchX(void) {
+int TouchScreen_kbv::readTouchX(void)
+{
     pinMode(_yp, INPUT);
     pinMode(_ym, INPUT);
     digitalWrite(_yp, LOW);
@@ -139,11 +149,11 @@ int TouchScreen_kbv::readTouchX(void) {
     pinMode(_xm, OUTPUT);
     digitalWrite(_xm, LOW);
 
-    return (1023 - (analogRead(_yp)) ADC_ADJUST);
+    return (1023 - (analogRead(_yp))ADC_ADJUST);
 }
 
-
-int TouchScreen_kbv::readTouchY(void) {
+int TouchScreen_kbv::readTouchY(void)
+{
     pinMode(_xp, INPUT);
     pinMode(_xm, INPUT);
     digitalWrite(_xp, LOW);
@@ -154,11 +164,11 @@ int TouchScreen_kbv::readTouchY(void) {
     pinMode(_ym, OUTPUT);
     digitalWrite(_ym, LOW);
 
-    return (1023 - (analogRead(_xm)) ADC_ADJUST);
+    return (1023 - (analogRead(_xm))ADC_ADJUST);
 }
 
-
-uint16_t TouchScreen_kbv::pressure(void) {
+uint16_t TouchScreen_kbv::pressure(void)
+{
     // Set X+ to ground
     pinMode(_xp, OUTPUT);
     digitalWrite(_xp, LOW);
@@ -178,4 +188,3 @@ uint16_t TouchScreen_kbv::pressure(void) {
 
     return (1023 - (z2 - z1));
 }
-
